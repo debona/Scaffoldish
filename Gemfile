b84fbe717c65023d1418345a50c2648016ef9d75
Thomas DE BONA <thomas.debona@gmail.com>
2014-11-28 08:48:28 +0100

    Update bundle gems

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ end
 
 group :test do
   gem 'rspec'
+  gem 'rspec-its'
   gem 'rspec-mocks'
   gem 'coveralls', require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
+require 'rspec/its'
+
 require 'coveralls'
 Coveralls.wear!
 
