6e7429595f986f69d6fb5ae03110f9687f32f19d
Thomas DE BONA <thomas.debona@gmail.com>
2014-12-03 22:47:29 +0100

    Create a method to generate a file from a template & data

--- lib/scaffoldish/scaffold.rb
+++ lib/scaffoldish/scaffold.rb
@@ -1,4 +1,8 @@
 
+require 'fileutils'
+
+require 'scaffoldish/application'
+
 module Scaffoldish
 
   class Scaffold
@@ -12,6 +16,22 @@ module Scaffoldish
 
     def run(*args)
       instance_exec(args, &block)
+    end
+
+    def generate(template_path, output_path, data)
+      template = File.read(File.join(Application.instance.templates_root, template_path))
+      renderer = ERB.new(template)
+
+      result = renderer.result(data.instance_eval { binding })
+
+      absolute_output_path = File.join(Application.instance.project_root, output_path)
+
+      dir = File.dirname(absolute_output_path)
+      FileUtils.mkpath(dir)
+
+      File.open(absolute_output_path, 'w') do |file|
+        file.write(result)
+      end
     end
 
   end
--- spec/scaffold_spec.rb
+++ spec/scaffold_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
+require 'ostruct'
+require 'fileutils'
+
 require 'scaffoldish/scaffold'
 
 describe Scaffoldish::Scaffold do
@@ -23,6 +26,48 @@ describe Scaffoldish::Scaffold do
 
     it { should be_a String }
     it { should == expected_result }
+  end
+
+  describe '#generate' do
+    project_root = File.join(File.dirname(__FILE__), 'fixtures', 'example')
+    templates_root = File.join(project_root, 'templates')
+
+    data = OpenStruct.new(words: 'one two three four!')
+    expected_output = "The following words should be generated: one two three four!"
+
+    before(:all) do
+      @scaffold = Scaffoldish::Scaffold.new(:scaffold_name) { }
+    end
+
+    after(:all) do
+      # TODO delete files
+    end
+
+    before do
+      Scaffoldish::Application.instance.stub(:project_root) { project_root }
+      Scaffoldish::Application.instance.stub(:templates_root) { templates_root }
+    end
+
+    it 'should create necessary directories' do
+      output_path = File.join('to_delete', 'test', 'test.txt')
+      absolute_output_path = File.join(project_root, output_path)
+
+      @scaffold.generate('test.txt.erb', output_path, data)
+      File.exist?(absolute_output_path).should == true
+
+      FileUtils.rm_r(File.join(project_root, 'to_delete'))
+    end
+
+    it 'should render the template and write the output in a file' do
+      absolute_output_path = File.join(project_root, 'test.txt')
+
+      @scaffold.generate('test.txt.erb', 'test.txt', data)
+      output = File.open(absolute_output_path).read
+      output.chomp.should == expected_output
+
+      File.delete(absolute_output_path)
+    end
+
   end
 
 end
